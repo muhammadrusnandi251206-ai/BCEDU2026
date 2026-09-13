4a870aa6c21e1d47103e78fe1c4c52b401cd1343
muhammadrusnandi251206-ai <muhammadrusnandi251206@gmail.com>
2026-09-13 09:25:50 +0700

    update API ke PythonAnywhere

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const API_URL = "http://127.0.0.1:5000";
+const API_URL = "https://nandi.pythonanywhere.com";
 
 function App() {
   const [halaman, setHalaman] = useState("beranda");
